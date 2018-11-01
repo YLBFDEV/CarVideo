c89e1bc4905e70fd22dae138bfefb0f86619b2e5
ylbf <ylbf_dev@qq.com>
2018-11-01 17:17:33 +0800

    打包

--- app/src/main/java/com/gjjx/carvideo/MainActivity.java
+++ app/src/main/java/com/gjjx/carvideo/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity
 
             ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                     this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawer.setDrawerListener(toggle);
+            drawer.addDrawerListener(toggle);
             toggle.syncState();
         } else {
             KLog.e("toolbar is null");
